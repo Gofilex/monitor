d0c2a8c39e074d64b77452e634e6e399322ee47d
Mart Jansink <mart@gofilex.nl>
2017-10-10 14:05:49 +0200

    try one more time

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 // make the cache of required modules available to code e.g. wrapped by Meteor
 // that can't reach the native require method
-console.log( require );
-module.exports = function() {
-  return require.cache;
-};
+module.exports = require.cache;
