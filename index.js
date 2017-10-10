5f850c8bf30453ac5d237eda81fcaeecfe372954
Mart Jansink <mart@gofilex.nl>
2017-10-10 14:03:16 +0200

    use a function to do so

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // make the cache of required modules available to code e.g. wrapped by Meteor
 // that can't reach the native require method
-module.exports = require.cache;
+console.log( require );
+module.exports = function() {
+  return require.cache;
+};
