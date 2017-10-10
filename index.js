e060a03427f346a2184c35c763b175adcbabde27
Mart Jansink <mart@gofilex.nl>
2017-10-10 13:58:42 +0200

    export require.cache

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log( "monitored!" );
+// make the cache of required modules available to code e.g. wrapped by Meteor
+// that can't reach the native require method
+module.exports = require.cache;
